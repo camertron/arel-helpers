20cf2220fca00c03b96053e1185c645c04bfb865
Cameron Dutro <cdutro@twitter.com>
2014-05-04 14:37:57 -0700

    Adding support for rails4

--- lib/arel-helpers/join_association.rb
+++ lib/arel-helpers/join_association.rb
@@ -6,9 +6,19 @@ module ArelHelpers
     # activerecord uses JoinDependency to automagically generate inner join statements for
     # any type of association (belongs_to, has_many, and has_and_belongs_to_many).
     # For example, for HABTM associations, two join statements are required.
-    # This method encapsulates that functionality and returns a SelectManager for chaining.
+    # This method encapsulates that functionality and yields an intermediate object for chaining.
     # It also allows you to use an outer join instead of the default inner via the join_type arg.
-    def join_association(table, association, join_type = Arel::InnerJoin)
+    def join_association(table, association, join_type = Arel::InnerJoin, &block)
+      if ActiveRecord::VERSION::STRING >= '4.1.0'
+        join_association_4_1(table, association, join_type, &block)
+      else
+        join_association_3_1(table, association, join_type, &block)
+      end
+    end
+
+    private
+
+    def join_association_3_1(table, association, join_type)
       associations = association.is_a?(Array) ? association : [association]
       join_dependency = ActiveRecord::Associations::JoinDependency.new(table, associations, [])
       manager = Arel::SelectManager.new(table)
@@ -26,6 +36,21 @@ module ArelHelpers
         else
           assoc
         end
+      end
+    end
+
+    def join_association_4_1(table, association, join_type)
+      associations = association.is_a?(Array) ? association : [association]
+      join_dependency = ActiveRecord::Associations::JoinDependency.new(table, associations, [])
+
+      join_dependency.join_constraints([]).map do |constraint|
+        right = if block_given?
+          yield constraint.left.name.to_sym, constraint.right
+        else
+          constraint.right
+        end
+
+        join_type.new(constraint.left, right)
       end
     end
 
