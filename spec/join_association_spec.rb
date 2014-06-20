f6490f53618ee88ce6d755246b0d5d11a059449d
Cameron Dutro <camertron@gmail.com>
2014-06-20 16:27:49 -0700

    Adding an ActiveSupport::Concern for join_association so there are more ways to use it besides just a semi-global method

--- spec/join_association_spec.rb
+++ spec/join_association_spec.rb
@@ -2,22 +2,20 @@
 
 require 'spec_helper'
 
-describe ArelHelpers::JoinAssociation do
-  include ArelHelpers::JoinAssociation
-
+describe ArelHelpers do
   describe "#join_association" do
     it "should work for a directly associated model" do
-      Post.joins(join_association(Post, :comments)).to_sql.should ==
+      Post.joins(ArelHelpers.join_association(Post, :comments)).to_sql.should ==
         'SELECT "posts".* FROM "posts" INNER JOIN "comments" ON "comments"."post_id" = "posts"."id"'
     end
 
     it "should work with an outer join" do
-      Post.joins(join_association(Post, :comments, Arel::OuterJoin)).to_sql.should ==
+      Post.joins(ArelHelpers.join_association(Post, :comments, Arel::OuterJoin)).to_sql.should ==
         'SELECT "posts".* FROM "posts" LEFT OUTER JOIN "comments" ON "comments"."post_id" = "posts"."id"'
     end
 
     it "should allow adding additional join conditions" do
-      Post.joins(join_association(Post, :comments) do |assoc_name, join_conditions|
+      Post.joins(ArelHelpers.join_association(Post, :comments) do |assoc_name, join_conditions|
         join_conditions.and(Comment[:id].eq(10))
       end).to_sql.should ==
         'SELECT "posts".* FROM "posts" INNER JOIN "comments" ON "comments"."post_id" = "posts"."id" AND "comments"."id" = 10'
@@ -25,19 +23,19 @@ describe ArelHelpers::JoinAssociation do
 
     it "should work for two models, one directly associated and the other indirectly" do
       Post
-        .joins(join_association(Post, :comments))
-        .joins(join_association(Comment, :author))
+        .joins(ArelHelpers.join_association(Post, :comments))
+        .joins(ArelHelpers.join_association(Comment, :author))
         .to_sql.should ==
           'SELECT "posts".* FROM "posts" INNER JOIN "comments" ON "comments"."post_id" = "posts"."id" INNER JOIN "authors" ON "authors"."comment_id" = "comments"."id"'
     end
 
     it "should be able to handle multiple associations" do
-      Post.joins(join_association(Post, [:comments, :favorites])).to_sql.should ==
+      Post.joins(ArelHelpers.join_association(Post, [:comments, :favorites])).to_sql.should ==
         'SELECT "posts".* FROM "posts" INNER JOIN "comments" ON "comments"."post_id" = "posts"."id" INNER JOIN "favorites" ON "favorites"."post_id" = "posts"."id"'
     end
 
     it "should yield once for each association" do
-      Post.joins(join_association(Post, [:comments, :favorites]) do |assoc_name, join_conditions|
+      Post.joins(ArelHelpers.join_association(Post, [:comments, :favorites]) do |assoc_name, join_conditions|
         case assoc_name
           when :favorites
             join_conditions.or(Favorite[:amount].eq("lots"))
@@ -47,5 +45,16 @@ describe ArelHelpers::JoinAssociation do
       end).to_sql.should ==
         'SELECT "posts".* FROM "posts" INNER JOIN "comments" ON "comments"."post_id" = "posts"."id" AND "comments"."text" = \'Awesome post!\' INNER JOIN "favorites" (ON "favorites"."post_id" = "posts"."id" OR "favorites"."amount" = \'lots\')'
     end
+  end
+end
+
+describe ArelHelpers::JoinAssociation do
+  class AssocPost < Post
+    include ArelHelpers::JoinAssociation
+  end
+
+  it "should provide the join_association method and use the parent class as the model to join on" do
+    AssocPost.joins(AssocPost.join_association(:comments)).to_sql.should ==
+      'SELECT "posts".* FROM "posts" INNER JOIN "comments" ON "comments"."post_id" = "posts"."id"'
   end
 end
