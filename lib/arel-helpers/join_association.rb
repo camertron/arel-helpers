f167ffbe561cec75671e18d308a7ca5ef511fcf2
Cameron Dutro <camertron@gmail.com>
2018-05-09 20:16:43 -0700

    Add join association support for rails 5.2

--- lib/arel-helpers/join_association.rb
+++ lib/arel-helpers/join_association.rb
@@ -18,7 +18,9 @@ module ArelHelpers
       # This method encapsulates that functionality and yields an intermediate object for chaining.
       # It also allows you to use an outer join instead of the default inner via the join_type arg.
       def join_association(table, association, join_type = Arel::Nodes::InnerJoin, options = {}, &block)
-        if ActiveRecord::VERSION::STRING >= '5.0.0'
+        if ActiveRecord::VERSION::STRING >= '5.2.0'
+          join_association_5_2(table, association, join_type, options, &block)
+        elsif ActiveRecord::VERSION::STRING >= '5.0.0'
           join_association_5_0(table, association, join_type, options, &block)
         elsif ActiveRecord::VERSION::STRING >= '4.2.0'
           join_association_4_2(table, association, join_type, options, &block)
@@ -149,6 +151,35 @@ module ArelHelpers
         end
 
         join_strings.join(' ')
+      end
+
+      def join_association_5_2(table, association, join_type, options = {})
+        aliases = options.fetch(:aliases, [])
+        associations = association.is_a?(Array) ? association : [association]
+
+        alias_tracker = ActiveRecord::Associations::AliasTracker.create(
+          table.connection, table.name, {}
+        )
+
+        join_dependency = ActiveRecord::Associations::JoinDependency.new(
+          table, table.arel_table, associations, alias_tracker
+        )
+
+        constraints = join_dependency.join_constraints([], join_type)
+
+        constraints.map do |join|
+          right = if block_given?
+            yield join.left.name.to_sym, join.right
+          else
+            join.right
+          end
+
+          if found_alias = find_alias(join.left.name, aliases)
+            join.left.table_alias = found_alias.name
+          end
+
+          join_type.new(join.left, right)
+        end
       end
 
       private
